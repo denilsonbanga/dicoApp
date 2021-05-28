e49a9b88999057c97d1cba5bd90cbe9fb6e606cb
denilsonbanga <denilsonbanga13@gmail.com>
2021-05-28 08:51:01 +0100

    vozFeita

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, Text} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home, Traducao,inicial } from './views'
+
+export default function App() {
+  const Stack = createStackNavigator();
+  
+  return (
+
+       <NavigationContainer>
+        <Stack.Navigator>
+
+        <Stack.Screen name="inicial"
+          component={inicial}
+          options={{ 
+            title: "Dicionário",
+            headerTintColor:'#333',
+            headerStyle:{backgroundColor:'#da2'},
+            headerTitleStyle:{ fontWeight:'bold', alignSelf:'center'} }}
+          
+        />
+
+        <Stack.Screen name="Palavras"
+          component={Home}
+          options={{
+            headerShown:false, 
+           title: "Palavras",
+           headerTintColor:'#333',
+           headerStyle:{backgroundColor:'#da2'},
+           headerTitleStyle:{ fontWeight:'bold', alignSelf:'center'} }}
+        /> 
+         <Stack.Screen name="Traducao" 
+        component={Traducao} 
+        options={{title: "Tradução", 
+        headerTintColor:'#333',
+        headerStyle:{backgroundColor:'#da2'},
+        headerTitleStyle:{ fontWeight:'bold', alignSelf:'center'} }        
+        } /> 
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
